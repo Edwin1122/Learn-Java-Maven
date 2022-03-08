cf4203eb33beefdff75b3847e083dcf3022e78bf
Edwin1122 <40655175+Edwin1122@users.noreply.github.com>
2022-03-08 15:56:32 +0800

    Interruption demo add bad way example

--- src/main/java/ThreadDemo/Interrupt/Cancelling/PrimeProducer.java
+++ src/main/java/ThreadDemo/Interrupt/Cancelling/PrimeProducer.java
@@ -7,6 +7,7 @@ import java.util.concurrent.*;
 
 public class PrimeProducer extends Thread {
     private final BlockingQueue<BigInteger> queue;
+//    private volatile boolean cancelled = false;
 
     PrimeProducer(BlockingQueue<BigInteger> queue) {
         this.queue = queue;
@@ -15,16 +16,20 @@ public class PrimeProducer extends Thread {
     public void run() {
         try {
             BigInteger p = BigInteger.ONE;
+//            while (!cancelled) //使用这种方式判断很可能卡在queue.put的阻塞操作中而使得取消无效
             while (!Thread.currentThread().isInterrupted())
                 queue.put(p = p.nextProbablePrime());
         } catch (InterruptedException e) {
-//            e.printStackTrace();
+            e.printStackTrace();
             /* Allow thread to exit */
         }
     }
 
     public void cancel() {
-        Thread.currentThread().interrupt();
+//        Thread.currentThread().interrupt();
+        interrupt();
+//        cancelled = true;
+
     }
 
     public synchronized BlockingQueue<BigInteger> get() {//同步锁, 监视器为当前类对象
