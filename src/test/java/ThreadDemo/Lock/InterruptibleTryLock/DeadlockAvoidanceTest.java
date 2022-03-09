f31789968e67945d4992d93895d7b5283af87d69
Edwin1122 <40655175+Edwin1122@users.noreply.github.com>
2022-03-09 18:43:07 +0800

    DeadlockAvoidance

---
+++ src/test/java/ThreadDemo/Lock/InterruptibleTryLock/DeadlockAvoidanceTest.java
@@ -0,0 +1,16 @@
+package ThreadDemo.Lock.InterruptibleTryLock;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeadlockAvoidanceTest {
+
+    @Test
+    void transferMoney() {
+
+//        DeadlockAvoidance deadlockAvoidance = new DeadlockAvoidance();
+//        DeadlockAvoidance.Account account = new DeadlockAvoidance.Account();
+//        deadlockAvoidance.transferMoney()
+    }
+}
