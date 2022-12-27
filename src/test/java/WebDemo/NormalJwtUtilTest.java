9f5badabc997f90144e287887fe1b4f0eaf68e9f
edwin <edwinluo1122@gmail.com>
2022-12-27 12:28:01 +0800

    normal jwt demo

---
+++ src/test/java/WebDemo/NormalJwtUtilTest.java
@@ -0,0 +1,28 @@
+package WebDemo;
+
+import cn.hutool.json.JSONObject;
+import org.junit.jupiter.api.Test;
+
+
+class NormalJwtUtilTest {
+
+    @Test
+    void verifyToken(){
+
+        JSONObject subjectJson = new JSONObject();
+        subjectJson.put("userId", 8888);
+        subjectJson.put("name", "ylc");
+
+        String token = NormalJwtUtil.createToken(subjectJson);
+        System.out.println("token:" + token);
+        System.out.println("===================");
+
+        System.out.println("Verify result: " + NormalJwtUtil.verifyToken(token));
+        System.out.println("===================");
+
+
+        System.out.println("Decode info: ");
+        NormalJwtUtil.decodeToken(token);
+
+    }
+}
