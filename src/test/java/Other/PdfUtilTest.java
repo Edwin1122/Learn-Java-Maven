19c74120d7fe0fd57bd2793547cc28171cda9082
edwin <edwinluo1122@gmail.com>
2022-12-27 12:11:03 +0800

    add Pdf Util

---
+++ src/test/java/Other/PdfUtilTest.java
@@ -0,0 +1,15 @@
+package Other;
+
+import Other.pdf.PdfUtil;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+class PdfUtilTest {
+
+    @Test
+    void generatePdf() throws IOException {
+
+        PdfUtil.generatePdf();
+    }
+}
