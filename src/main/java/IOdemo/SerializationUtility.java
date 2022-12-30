efcbd6b64ed93a0d8bfbb99cc25827d0e031fa6c
edwin <edwinluo1122@gmail.com>
2022-12-30 14:22:23 +0800

    IO stream serialization demo

--- src/main/java/IOdemo/SerializationUtility.java
+++ src/main/java/IOdemo/SerializationUtility.java
@@ -37,4 +37,8 @@ public class SerializationUtility {
                 baos.toByteArray()
         );
     }
+
+    public static void StringBytesConvertion() {
+        byte[] bytes = "abcde".getBytes();
+    }
 }
